541e13c83d6fef86c11491d6ec995dc89cee04e0
Muklasar <muklasar@gmail.com>
2023-08-23 17:57:26 +0530

    add show password list copy

--- src/components/DisplayPassword.jsx
+++ src/components/DisplayPassword.jsx
@@ -2,11 +2,11 @@ import CopyIcon from "./CopyIcon";
 const DisplayPassword = ({ state }) => {
   const { password, copyToClipboard } = state;
   return (
-    <div className="">
+    <div onClick={()=>copyToClipboard(password)}>
       <h5 className="mt-4">Your Generated Password</h5>
       <li className="mt-3">
         {password}
-        <span onClick={()=>copyToClipboard(password)}>
+        <span>
           {" "}
           <CopyIcon />
         </span>
